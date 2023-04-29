f99ad86f99fb171eaf52ce5f2e29f87231a5ae78
Sergio Terroso <sergio.terroso@gmail.com>
2023-04-29 11:48:16 -0600

    Added dotenv dependency

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
 
 import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT||8080;
